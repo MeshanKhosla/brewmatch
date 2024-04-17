1b6b4f636742ac8f4fa9a856427a84d1e8273578
Ofir Dvir <odvir@berkeley.edu>
2024-04-17 14:41:21 -0700

    modified discover page

--- src/app/add-cafe/page.tsx
+++ src/app/add-cafe/page.tsx
@@ -12,9 +12,6 @@ const Page = async () => {
 
   return (
     <div>
-      <div className="flex items-center justify-center bg-[#F7F0DD] text-black p-4 text-center py-12">
-        <h2 className="text-4xl text-center">ADD CAFE</h2>
-      </div>
       <CreateCafe />
     </div>
   )
