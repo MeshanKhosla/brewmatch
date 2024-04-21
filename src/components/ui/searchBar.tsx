22efe1220896483d1523fe2fd86e7b367e747006
Meshan Khosla <mkhosla@berkeley.edu>
2024-04-21 11:04:08 -0700

    Fix build errors

--- src/components/ui/searchBar.tsx
+++ src/components/ui/searchBar.tsx
@@ -7,10 +7,9 @@ import {
     CommandItem,
     CommandList,
     CommandSeparator,
-  } from "~/components/ui/command";
+} from "~/components/ui/command";
 
 import { useState } from "react";
-import { redirect } from 'next/navigation'
 import { useRouter } from 'next/navigation';
 
 
@@ -18,17 +17,17 @@ export function SearchBar() {
     const [open, setOpen] = useState<boolean>(false);
     const router = useRouter();
     return (<Command className="">
-            <CommandInput placeholder="Search a Cafe..." onSelect={() => setOpen(true)}/>
-            {open && <CommandList>
-                <CommandEmpty>No results found.</CommandEmpty>
-                <CommandGroup heading="Suggestions">
-                    <CommandItem>Philz</CommandItem>
-                </CommandGroup>
-                <CommandSeparator />
-                <CommandGroup heading="Cafes">
-                    <CommandItem onSelect={(value) => router.push("/cafe/SoDoI")}>SoDoI</CommandItem>
-                    <CommandItem onSelect={(value) => router.push("/cafe/Blue%20Bottle")}>Blue Bottle</CommandItem>
-                </CommandGroup>
-            </CommandList>}
-        </Command>);
-    }
+        <CommandInput placeholder="Search a Cafe..." onSelect={() => setOpen(true)} />
+        {open && <CommandList>
+            <CommandEmpty>No results found.</CommandEmpty>
+            <CommandGroup heading="Suggestions">
+                <CommandItem>Philz</CommandItem>
+            </CommandGroup>
+            <CommandSeparator />
+            <CommandGroup heading="Cafes">
+                <CommandItem onSelect={(value) => router.push("/cafe/SoDoI")}>SoDoI</CommandItem>
+                <CommandItem onSelect={(value) => router.push("/cafe/Blue%20Bottle")}>Blue Bottle</CommandItem>
+            </CommandGroup>
+        </CommandList>}
+    </Command>);
+}
