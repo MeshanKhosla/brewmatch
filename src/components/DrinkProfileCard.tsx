aa96f033f240d236444c06df8dd71ed4907b1c12
Cynthia Lan <cynthialan@berkeley.edu>
2024-04-25 14:42:01 -0700

    final put buttons to edge

--- src/components/DrinkProfileCard.tsx
+++ src/components/DrinkProfileCard.tsx
@@ -40,12 +40,13 @@ export function DrinkProfileCard(props: DrinkProfileCardProps) {
       onClick={onCardClick}
       className={handleProfileSelection ? "hover:cursor-pointer" : ""}
     >
-      <div className="flex size-1 w-full items-center justify-between"></div>
+      <div className="flex w-full items-center justify-between"></div>
       <div
       className="group relative cursor-pointer bg-white overflow-hidden transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl mx-auto max-w rounded-lg">
       <span className="absolute top-0 z-0 h-0.5 w-0.5 rounded-full bg-lime-600/50 transition-all duration-300 group-hover:scale-[800]"></span>
       <div className="relative z-10 mx-auto max-w-md">
           <span className="grid h-full w-full rounded-lg transition-all duration-300 group-hover:bg-lime-500/50"></span>
+          </div>
       <CardHeader>
         {canEdit && (
           <div className="flex size-1 w-full items-center justify-between space-x-2 pb-5">
@@ -75,7 +76,6 @@ export function DrinkProfileCard(props: DrinkProfileCardProps) {
           </CardDescription>
         </div>
       </CardHeader>
-      </div>
       </div>
     </Card>
   );
