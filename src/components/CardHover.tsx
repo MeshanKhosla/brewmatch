2bb62981484f1b04921930e0660b04b18a3be33d
Meshan Khosla <mkhosla@berkeley.edu>
2024-04-27 14:20:19 -0700

    Remove hover on mobile

--- src/components/CardHover.tsx
+++ src/components/CardHover.tsx
@@ -1,7 +1,7 @@
 /** Hover effect on a card */
 const CardHover = () => {
   return (
-    <div>
+    <div className="invisible md:visible">
       <span className="absolute top-0 -z-10 h-0.5 w-0.5 rounded-full bg-lime-600/50 transition-all duration-300 group-hover:scale-[800]"></span>
       <div className="relative mx-auto max-w-md">
         <span className="grid h-full w-full rounded-lg transition-all duration-300 group-hover:bg-lime-500/50"></span>
