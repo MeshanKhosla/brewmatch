263a604dba2587d7204d4c8cc01266a0d7f92eea
Ofir Dvir <odvir@berkeley.edu>
2024-04-29 15:48:03 -0700

    order history

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,9 @@
+import Link from "next/link";
 import { getServerSession } from "next-auth";
+import CardHover from "~/components/CardHover";
 import CreateDrinkProfile from "~/components/CreateDrinkProfile";
 import { DrinkProfileCard } from "~/components/DrinkProfileCard";
+import { Card, CardDescription, CardHeader, CardTitle } from "~/components/ui/card";
 import { getDrinkProfilesByCreator, getOrdersByCreator } from "~/queries";
 import { authOptions } from "~/server/auth";
 
@@ -14,15 +17,7 @@ const Page = async () => {
   }
 
   const myDrinkProfiles = await getDrinkProfilesByCreator(session.user.id);
-  const myDrinkOrders = await getOrdersByCreator(session.user.id);
-  const firstOrder = myDrinkOrders[0];
-  const firstOrderDrink = firstOrder?.drink;
-  const firstOrderCafe = firstOrderDrink?.cafe;
-  console.log({
-    firstOrder,
-    firstOrderDrink,
-    firstOrderCafe,
-  });
+  const orders = await getOrdersByCreator(session.user.id);
 
   return (
     <div className="flex flex-col gap-4 pt-4">
@@ -41,6 +36,27 @@ const Page = async () => {
         </div>
       )}
       <CreateDrinkProfile />
+      <h2 className="text-4xl font-semibold">Order History</h2>
+      <div className="grid grid-cols-1 gap-3 pt-4 md:grid-cols-2">
+          {orders.map((order) => (
+            <Link href={`/cafe/${order.drink.cafe.name}`} key={order.drink.cafe.id}>
+                  <Card key={order.id} className="h-full">
+                    <div className="max-w group relative mx-auto h-full cursor-pointer overflow-hidden rounded-lg bg-white transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl">
+                      <CardHover />
+                      <CardHeader className="h-full">
+                        <div className="grid h-full grid-rows-[max-content_1fr]">
+                          <CardTitle>{order.drink.name}</CardTitle>
+                          <CardTitle className="text-lg">{order.drink.cafe.name}</CardTitle> 
+                          <CardDescription>{`ordered on: ${
+                            new Date(order.createdAt).toLocaleDateString()
+                          }`}</CardDescription>
+                        </div>
+                      </CardHeader>
+                    </div>
+                  </Card>
+            </Link>
+          ))}
+        </div>
     </div>
   );
 };
