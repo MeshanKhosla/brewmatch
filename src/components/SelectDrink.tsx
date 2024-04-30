e161067df6faa780290ffe02991c1bf10e9bdc04
Meshan Khosla <mkhosla@berkeley.edu>
2024-04-30 15:00:40 -0700

    Add flavor text

--- src/components/SelectDrink.tsx
+++ src/components/SelectDrink.tsx
@@ -11,11 +11,15 @@ type SelectDrinkProps = {
 };
 
 const SelectDrink = (props: SelectDrinkProps) => {
-  const { drinkRecommendations, recommendationReasons, handleDrinkSelection } = props;
+  const { drinkRecommendations, recommendationReasons, handleDrinkSelection } =
+    props;
   if (drinkRecommendations.length === 0 || recommendationReasons.length === 0) {
     return (
       <div>
         <div className="mb-4 text-3xl font-bold">Top 3 Recommendations</div>
+        <p className="mb-2 text-center font-light text-gray-500 transition-all duration-300">
+          Brewing up some recommendations for you...
+        </p>
         <LoadingCards cafe={false} />
       </div>
     );
@@ -23,18 +27,19 @@ const SelectDrink = (props: SelectDrinkProps) => {
 
   return (
     <div className="flex flex-col gap-2">
-      <h1 className="text-2xl font-semibold mb-4">Top 3 Recommendations</h1>
+      <h1 className="mb-4 text-2xl font-semibold">Top 3 Recommendations</h1>
       <div className="cols-1 md:rows-3 mb-5">
-        {recommendationReasons && drinkRecommendations.map((drink, index) => (
-          <DrinkCard
-            key={drink.drink.id}
-            drink={drink.drink}
-            score={drink.score}
-            reason={recommendationReasons[index]}
-            canEdit={false}
-            handleDrinkSelection={handleDrinkSelection}
-          />
-        ))}
+        {recommendationReasons &&
+          drinkRecommendations.map((drink, index) => (
+            <DrinkCard
+              key={drink.drink.id}
+              drink={drink.drink}
+              score={drink.score}
+              reason={recommendationReasons[index]}
+              canEdit={false}
+              handleDrinkSelection={handleDrinkSelection}
+            />
+          ))}
       </div>
     </div>
   );
